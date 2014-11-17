363d4f2701717b2272b6c7b53b88bacc34d5fd97
Jessica Mullison <jdmull@acm.org>
2014-11-17 17:17:51 -0500

    added makeCacheMatrix, cacheSolve
    added functions:
    makeCacheMatrix <- returns object that caches matrix & its inverse
    cacheSolve <- function that calculates inverse by reading it from cached
    object or using ginv & caching result

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cashematrix module - Create matrix inverses, cache results
+ 
+library(MASS)  ## library for function that calculates a general matrix inverse
 
-## Write a short comment describing this function
+## Create a matrix object that can cache its inverse
+makeCacheMatrix <- function(thisMatrix = matrix()) {
+    cachedInverse<-NULL
 
-makeCacheMatrix <- function(x = matrix()) {
-
+    ## property accessors, allowing get & set on origional matrix and inverse matrix
+    set <- function(matrixToAssign){
+        thisMatrix <- matrixToAssign
+        cachedInverse <-NULL
+    }
+    get <- function(){ thisMatrix }
+    setInverse <- function(inverse){ cachedInverse <- inverse }
+    getInverse <- function(){ cachedInverse }
+  
+    list(set = set, get = get, setInverse = setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## compute inverse of the matrix returned by makeCaseMatrix
+cacheSolve <- function(x) {    ## Return a matrix that is the inverse of 'x'
+  inverse <-x$getInverse()
+  if (is.null(inverse)){  ## If there was no cached inverse, calculate & cache it
+    xMatrix<-x$get()
+    inverse<-ginv(xMatrix)  ## calculation of inverse matrix
+    x$setInverse(inverse)   ## saving result
+  }
+  inverse;
 }
